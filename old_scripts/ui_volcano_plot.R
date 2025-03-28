436a0a6016a74d93787d0b31fc6c3eead853c105
Jyotirmoy Das <jyotirmoy21@gmail.com>
2025-03-28 07:50:45 +0100

    Delete old_scripts directory

--- old_scripts/ui_volcano_plot.R
+++
@@ -1,9 +0,0 @@
-volcano_plot_ui <- function() {
-  tagList(
-    selectInput("volcano_var", "Grouping Variable", choices = NULL),
-    radioButtons("volcano_var_type", "Variable Type", choices = c("Character", "Factor")),
-    actionButton("run_volcano", "Generate Volcano Plot", class = "btn-primary"),
-    plotOutput("volcano_plot"),
-    downloadButton("download_volcano", "Download Plot", class = "btn-success")
-  )
-}
